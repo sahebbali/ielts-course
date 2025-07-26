279ac21b5854b9a3a99e8b6af21865418220b763
sahebbali <saheb1996jmr@gmail.com>
2025-07-26 20:32:48 +0600

    fixed some issue

--- src/app/components/Trailer.tsx
+++ src/app/components/Trailer.tsx
@@ -1,14 +1,18 @@
-'use client';
-import React from 'react';
-import YouTube from 'react-youtube';
+"use client";
+import React from "react";
+import YouTube from "react-youtube";
 
 interface Props {
   url: string;
 }
 
 const Trailer: React.FC<Props> = ({ url }) => {
-  const videoId = url.split('v=')[1];
-  return <YouTube videoId={videoId} className="w-full" />;
+  const videoId = url.split("v=")[1];
+  const opts = {
+    width: "320", // Set your desired width
+    height: "180", // Set your desired height
+  };
+  return <YouTube videoId={videoId} opts={opts} />;
 };
 
 export default Trailer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,33 @@
-
-import './globals.css';
-import type { Metadata } from 'next';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: '10 Minute School',
-  description: 'Official website for 10 Minute School products',
+  title: "10 Minute School",
+  description: "Official website for 10 Minute School products",
 };
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="max-w-5xl mx-auto w-full">{children}</div>
+      </body>
     </html>
   );
 }
